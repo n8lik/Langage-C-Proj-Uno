e9f3d931d6e818fe47a948eb4c1dc362912c9bb6
meeaavv <alopes15@myges.fr>
2024-02-19 00:10:34 +0100

    Affichage des cartes sur plateau --multi oK

--- mode/multi.c
+++ mode/multi.c
@@ -55,12 +55,58 @@ int multi ()
     SDL_Flip(screen);
 
 
-
-
     //Code pour le jeu
-    //Initialisation des cartes
-    init_players(players, nb_players);
-    
+     init_players(players, nb_players);
+
+    // Déclaration et initialisation des variables
+    int current_player = 0; // ou choisir un joueur au hasard
+    int direction = 1;      // 1 pour sens horaire, -1 pour sens antihoraire
+    int nb_cards_to_draw = 0;
+
+    // Déclaration et initialisation de la variable top_card
+    card top_card;
+
+    // Création et mélange du deck
+    card *deck = create_deck();
+    int deck_size = 108;
+
+    display_deck(deck, deck_size);
+    shuffle_deck(deck, 108, &top_card);
+
+    //Taille du deck
+
+    distribute_cards(players, deck, nb_players);
+    display_cards(players, nb_players);
+
+    // Boucle du jeu
+    while (1)
+    {
+        play_turn(players, nb_players, deck, deck_size, &nb_cards_to_draw, &current_player, &direction, &nb_cards_to_draw, &top_card, screen);
+
+        // Vérifier si le joueur actuel a gagné
+        if (has_won(players[current_player]))
+        {
+            printf("%s a gagné!\n", players[current_player].name);
+            break;
+        }
+
+        // Passer au joueur suivant
+        current_player = (current_player + direction + nb_players) % nb_players;
+    }
+
+    // Calculer et enregistrer les scores
+    calculate_scores(players, nb_players);
+
+    // Libération de la mémoire
+    free(deck);
+    for (int i = 0; i < nb_players; i++)
+    {
+        free(players[i].cards);
+    }
+    free(players);
+
+    return 0;
+
 
 
     SDL_Event event;
--- rules.h
+++ rules.h
@@ -3,6 +3,7 @@
 
 #include "card.h"
 #include "player.h"
+#include <SDL/SDL.h>
 
 
 typedef struct GameState GameState;
@@ -18,7 +19,7 @@ int can_be_played(struct card card, struct card top_card);
 int has_won(player player);
 void apply_special_card_effect(struct card card, struct player *players, int nb_players, int *current_player, int *direction, int *nb_cards_to_draw, struct card *top_card, struct card *deck, int deck_size);
 
-void play_turn(player *players, int nb_players, card *deck, int deck_size, int *nb_cards_drawn, int *current_player, int *direction, int *nb_cards_to_draw, card *top_card);
+void play_turn(player *players, int nb_players, card *deck, int deck_size, int *nb_cards_drawn, int *current_player, int *direction, int *nb_cards_to_draw, card *top_card, SDL_Surface *screen);
 
 void calculate_scores(player *players, int nb_players);
 
